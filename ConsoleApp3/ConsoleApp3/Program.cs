da48de2dfda683d47bdf53ab468f1237505b60ab
Pedro Jorge Oliveira Braga <pjobraga98@gmail.com>
2022-07-18 22:25:39 -0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, prod;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            prod = A * B - (D * C);
+            Console.WriteLine("Diferenca = "prod);
+
+        }
+    }
+}
